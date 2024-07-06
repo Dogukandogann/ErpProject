cd9710ffc69cef54ed79f42c21f927487f872f5a
Dogukandogann <dogukn.dogann@gmail.com>
2024-07-06 13:33:08 +0300

    Initial commit with existing project files

---
+++ ERPServer/ERPServer.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace ERPServer.Application.Mapping
+{
+    public sealed class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+
+        }
+    }
+}
